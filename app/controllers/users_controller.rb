674a6b096356bf0fb0d00d8a669e6ac77b275e74
David Casper <davidcasper00@gmail.com>
2013-04-24 12:44:37 -0700

    Add admin self-destruction protection and disallow signed in users to make new accounts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, 	 only: [:edit, :update]
   before_filter :admin_user, 	 only: :destroy
+  before_filter :non_admin_user, only: [:new, :create]
 
   def show
     @user = User.find(params[:id])
@@ -40,13 +41,18 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
+    @user = User.find(params[:id])
+    if(current_user?(@user))
+      redirect_to root_path
+    else
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    end
   end
 
   private
-    
+
     def signed_in_user
       store_location
       redirect_to signin_url, notice: "Please sign in." unless signed_in?
@@ -58,6 +64,10 @@ class UsersController < ApplicationController
     end
 
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      redirect_to(root_path) unless current_user.admin? 
+    end
+
+    def non_admin_user
+      redirect_to(root_path) unless (signed_in? == false || current_user.admin? )
     end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -48,12 +48,30 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    describe "as admin" do
+      let(:admin) { FactoryGirl.create(:admin) }
+
+      before { sign_in admin }
+
+      describe "attempting to destroy own account" do
+        before { delete user_path(admin) }
+	specify { response.should redirect_to(root_path) }
+      end
+    end
+
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
 
       before { sign_in non_admin }
 
+      describe "trying to create a new user" do
+        before { visit new_user_path  }
+	specify { response.should redirect_to(root_path) }
+
+	before { post users_path }
+	specify { response.should redirect_to(root_path) }
+      end 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
 	specify { response.should redirect_to(root_path) }
